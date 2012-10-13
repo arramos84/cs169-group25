1b725af3ea78a059f96a1cdee9426c9303f3d408
Steve <stevenroger@comcast.net>
2012-10-13 15:14:14 -0700

    Added Users controller and configured rspec

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 CS169Group31::Application.routes.draw do
+  get "users/new"
+  
+  match '/signup',  to: 'users#new'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
