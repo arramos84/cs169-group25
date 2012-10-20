dd59cebb294a59a520c7f69f25637ca8c0d53ba1
Steve <stevenroger@comcast.net>
2012-10-19 23:01:31 -0700

    almost done with user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,20 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :first_name, :last_name
+  attr_accessible :email, :first_name, :last_name, :password, :password_confirmation
+  #magic to require a password, make sure passwords match, authenticate
+  has_secure_password
   
-  validates :email, presence: true
-  validates :first_name, presence: true
-  validates :last_name, presence: true
+  before_save { |user| user.email = email.downcase } #help ensure uniqueness
+  before_save :create_remember_token
+  
+  validates :last_name,  presence: true, length: { maximum: 50 }  
+  validates :first_name,  presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: {case_sensitive: false}
+  validates :password, presence: true, length: { minimum: 5 }
+  validates :password_confirmation, presence: true
+  
+  def self.get_full_name
+    return :first_name + " " + :last_name
+  end  
   
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,13 +2,21 @@ require 'spec_helper'
 
 describe User do
   
-  before { @user = User.new(first_name: "Charles", last_name: "Smith", email: "charlessmith@aol.com") }
+  before { @user = User.new(first_name: "Charles", last_name: "Smith", email: "charlessmith@aol.com", 
+    password: "12345678", password_confirmation: "12345678") }
+  
 
   subject { @user }
 
   it { should respond_to(:first_name) }
   it { should respond_to(:last_name) }
   it { should respond_to(:email) }
+  it { should respond_to(:password_digest) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
+  it { should respond_to(:authenticate) }
+  
+  #it { should be_valid }
    
   describe "when first name is not present" do
     before @user.first_name = " "
@@ -29,5 +37,35 @@ describe User do
     before @user.first_name = "a" * 51
     it { should_not be_valid }
   end
+  
+  describe "when last name is too long" do
+    before @user.last_name = "a" * 51
+    it { should_not be_valid}
+  end
+
+  describe "when email address is already taken" do
+    before do
+      user_with_same_email = @user.dup
+      user_with_same_email.save
+    end
+    it { should_not be_valid }
+  end
+  
+  describe "when password is not present" do
+    before { @user.password = @user.password_confirmation = " " }
+    it { should_not be_valid }
+  end
+  
+  describe "when password doesn't match confirmation" do
+    before { @user.password_confirmation = "mismatch" }
+    it { should_not be_valid }
+  end
+  
+  describe "when password confirmation is nil" do
+    before { @user.password_confirmation = nil }
+    it { should_not be_valid }
+  end
+  
+  
   
 end
