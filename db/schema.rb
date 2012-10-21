d7cdaf30ff4cc689d385c8410e3adbd2c6f6efcc
Arbong <arbong755@gmail.com>
2012-10-20 22:33:03 -0700

    added questions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121021022116) do
+ActiveRecord::Schema.define(:version => 20121021035354) do
+
+  create_table "survey", :force => true do |t|
+    t.integer  "ei"
+    t.integer  "ns"
+    t.integer  "ft"
+    t.integer  "jp"
+    t.string   "type"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "first_name"
