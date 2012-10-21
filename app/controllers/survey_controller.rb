95b0bdb940fdb6ddc3034d344c5605dfc4c5c98d
Steve <stevenroger@comcast.net>
2012-10-20 22:14:09 -0700

    changed survey controller

--- app/controllers/survey_controller.rb
+++ app/controllers/survey_controller.rb
@@ -1,9 +1,14 @@
-class PersonalityController < ApplicationController
+class SurveyController < ApplicationController
   
   def new
   end
 
   def create
+    if Survey.create_survey(params[:survey])
+      redirect_to root_path
+    else
+      render :new
+    end
   end
   
   def destroy
