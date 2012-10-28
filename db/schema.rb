087ef795f17c73319b323b0ff9b0526a3209c131
Steve <stevenroger@comcast.net>
2012-10-28 14:59:19 -0700

    added Profile model and controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,32 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121021205638) do
+ActiveRecord::Schema.define(:version => 20121028212625) do
+
+  create_table "profiles", :force => true do |t|
+    t.string   "step_1"
+    t.string   "step_2"
+    t.string   "step_3"
+    t.string   "step_4"
+    t.string   "step_5"
+    t.string   "video_link"
+    t.string   "body"
+    t.datetime "created_at",       :null => false
+    t.datetime "updated_at",       :null => false
+    t.string   "personality_type"
+  end
+
+  add_index "profiles", ["personality_type"], :name => "index_profiles_on_personality_type", :unique => true
+
+  create_table "survey", :force => true do |t|
+    t.integer  "ei"
+    t.integer  "ns"
+    t.integer  "ft"
+    t.integer  "jp"
+    t.string   "type"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "surveys", :force => true do |t|
     t.integer  "ei"
