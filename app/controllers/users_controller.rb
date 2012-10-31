bf5586bd17c93d1eb92228bbd27f8ce73dceda5c
Kunal Agarwal <kunala@berkeley.edu>
2012-10-31 16:54:27 -0700

    finished survey stuff

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,8 +12,17 @@ class UsersController < ApplicationController
     end
     @personality_db = Profile.find_by_personality_type(@user.survey.personality_type)
     #puts @personality_db.inspect
+    
     @video_url = /v=(.*)/.match(@personality_db.video_link)[1]
+    @body = @personality_db.body
+    @step_1 = @personality_db.step_1
+    @step_2 = @personality_db.step_2
+    @step_3 = @personality_db.step_3
+    @step_4 = @personality_db.step_4
+    @step_5 = @personality_db.step_5
+
   end
+
 
   def destroy
   end
