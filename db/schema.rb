cd214754ddc61898c017c8c80b523a5160711c92
arramos84 <arramos84@gmail.com>
2013-10-24 19:56:16 -0700

    migrated db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121205011617) do
+ActiveRecord::Schema.define(:version => 20131025024959) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -83,6 +83,7 @@ ActiveRecord::Schema.define(:version => 20121205011617) do
     t.integer  "user_id"
     t.datetime "created_at",       :null => false
     t.datetime "updated_at",       :null => false
+    t.text     "responses"
   end
 
   create_table "users", :force => true do |t|
