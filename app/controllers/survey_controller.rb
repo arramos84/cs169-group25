59b23c7cc1828ce6328e9c7d2b4888963f397af4
mikeymc <mccormack@berkeley.edu>
2013-11-07 16:11:13 -0800

    uncommented the test results response component

--- app/controllers/survey_controller.rb
+++ app/controllers/survey_controller.rb
@@ -27,12 +27,11 @@ class SurveyController < ApplicationController
 
     @survey = Survey.new(@survey_params)
     current_user.survey = @survey
-    ############## This is a bug ################
-    #if Survey.last_test_result != nil
-    #  @survey.responses = Survey.last_test_result
-    #  Survey.last_test_result = nil
-    #end
-    #############################################
+
+    if Survey.last_test_result != nil
+      @survey.responses = Survey.last_test_result
+      Survey.last_test_result = nil
+    end
 
     if @survey.save
       flash[:success] = "Welcome to LeadU!"
